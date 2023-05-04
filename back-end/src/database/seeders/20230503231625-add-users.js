3a649884515fde2d8c489b3e489cf10c1151e7e8
josiel <josiel.jcc@hotmail.com>
2023-05-04 15:45:43 -0300

    fix: user seeds

--- back-end/src/database/seeders/20230503231625-add-users.js
+++ back-end/src/database/seeders/20230503231625-add-users.js
@@ -2,15 +2,24 @@ module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('users', [
       {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-        role: 'admin',
+        id: 1,
+        name: 'Delivery App Admin',
+        email: 'adm@deliveryapp.com',
+        password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+        role: 'administrator',
       },
       {
-        name: 'Jane Doe',
-        email: 'janedoe@example.com',
-        password: 'password456',
+        id: 2,
+        name: 'Fulana Pereira',
+        email: 'fulana@deliveryapp.com',
+        password: '3c28d2b0881bf46457a853e0b07531c6',
+        role: 'seller',
+      },
+      {
+        id: 3,
+        name: 'Cliente Zé Birita',
+        email: 'zebirita@email.com',
+        password: '1c37466c159755ce1fa181bd247cb925',
         role: 'customer',
       },
     ]);
