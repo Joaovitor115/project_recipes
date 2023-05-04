9eea13816303993f2781a020f0dc2445f066a3e8
josiel <josiel.jcc@hotmail.com>
2023-05-04 15:53:25 -0300

    fix: using redirect route / to /login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import { /* BrowserRouter */ Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom';
 import Login from './pages/Login';
 
 class App extends React.Component {
@@ -11,8 +12,8 @@ class App extends React.Component {
         {/* <img> Logo </img> */}
         <p>Trybeer</p>
         <Switch>
-          <Route exact path="/" component={ Login } />
           <Route exact path="/login" component={ Login } />
+          <Redirect exact from="/" to="/login" />
         </Switch>
       </div>
     );
