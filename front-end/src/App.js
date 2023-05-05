ec9fb045df7e0b19a894fb5ca7dddd85b41ee392
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-05-05 17:50:48 -0300

    pagina pessoa vendedora

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { /* BrowserRouter */ Switch, Route } from 'react-router-dom';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom';
 import Login from './pages/Login';
+import SellerOrders from './pages/SellerOrders';
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/login" component={ Login } />
           <Redirect exact from="/" to="/login" />
+          <Route exact path="/seller/orders" component={ SellerOrders } />
+
         </Switch>
       </div>
     );
