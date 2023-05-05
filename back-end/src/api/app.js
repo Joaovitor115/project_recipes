5b8bf7fb3615f69c00dae60ba3a7ed77ff4df7c1
josiel <josiel.jcc@hotmail.com>
2023-05-05 15:18:17 -0300

    feat: create a post requisition on front to /login in backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const loginRouter = require('./routes/loginRouter');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/login', loginRouter);
 
