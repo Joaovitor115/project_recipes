f2a75915d6067a636691124309f8a858fa247d4f
Priscila <priscilasartori2@gmail.com>
2023-05-06 00:09:44 -0300

    feat: Implementação da tela 'customer/products'

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import { /* BrowserRouter */ Switch, Route } from 'react-router-dom';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom';
 import Login from './pages/Login';
 import SellerOrders from './pages/SellerOrders';
+import CustomerProducts from './pages/CustomerProducts';
 
 class App extends React.Component {
   render() {
@@ -13,10 +14,10 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/login" component={ Login } />
           {/* Rota Ficticia para passar no requisito 05 - temporario */}
-          <Route exact path="/customer/products" component={ Login } />
+          {/* <Route exact path="/customer/products" component={ Login } /> */}
           <Redirect exact from="/" to="/login" />
           <Route exact path="/seller/orders" component={ SellerOrders } />
-
+          <Route exact path="/customer/products" component={ CustomerProducts } />
         </Switch>
       </div>
     );
