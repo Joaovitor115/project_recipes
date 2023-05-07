cfa0990e229b9b58cdad7380995af9ba4eb43cf1
Priscila <priscilasartori2@gmail.com>
2023-05-07 17:12:04 -0300

    feat: Desenvolvimento da tela de produtos do cliente

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import products from '../tests/mocks/products.mocks';
 
 function CustomerProducts() {
   return (
@@ -19,6 +20,38 @@ function CustomerProducts() {
           </li>
         </ul>
       </nav>
+      <div>
+        {
+          products.map(({ id, name, price, urlImage }) => (
+            <div key={ id }>
+              <img
+                src={ urlImage }
+                alt={ name }
+                data-testid={ `customer_products__img-card-bg-image-${id}` }
+              />
+              <p data-testid={ `customer_products__element-card-title-${id}` }>{name}</p>
+              <p data-testid={ `customer_products__element-card-price-${id}` }>{price}</p>
+              <button
+                type="button"
+                data-testid={ `customer_products__button-card-rm-item-${id}` }
+              >
+                -
+              </button>
+              <input
+                type="text"
+                data-testid={ `customer_products__input-card-quantity-${id}` }
+                // {quantities}
+              />
+              <button
+                type="button"
+                data-testid={ `customer_products__button-card-add-item-${id}` }
+              >
+                +
+              </button>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
