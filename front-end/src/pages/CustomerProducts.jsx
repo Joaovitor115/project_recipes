b9ce903123dbff672ffdf7919433ea09f08b1835
Priscila <priscilasartori2@gmail.com>
2023-05-09 09:01:38 -0300

    feat: Implementando o incremento e o decremento da quantidade do produto

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import products from '../tests/mocks/products.mocks';
 import NavBar from '../components/NavBar';
 
 function CustomerProducts() {
+  const [quantities, setQuantities] = useState(0);
+
+  const decrement = () => {
+    setQuantities(quantities - 1);
+  };
+
+  const increment = () => {
+    setQuantities(quantities + 1);
+  };
+
   return (
     <div>
       <NavBar />
@@ -20,17 +30,19 @@ function CustomerProducts() {
               <button
                 type="button"
                 data-testid={ `customer_products__button-card-rm-item-${id}` }
+                onClick={ decrement }
               >
                 -
               </button>
               <input
                 type="text"
                 data-testid={ `customer_products__input-card-quantity-${id}` }
-                // {quantities}
+                value={ quantities }
               />
               <button
                 type="button"
                 data-testid={ `customer_products__button-card-add-item-${id}` }
+                onClick={ increment }
               >
                 +
               </button>
