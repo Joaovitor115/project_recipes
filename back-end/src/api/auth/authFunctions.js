4140d9f7e3cc5580cd00c0ddcc282b289a6c90a8
Priscila <priscilasartori2@gmail.com>
2023-05-09 19:46:53 -0300

    feat: Implementando a leitura do jwtKey

--- back-end/src/api/auth/authFunctions.js
+++ back-end/src/api/auth/authFunctions.js
@@ -1,15 +1,20 @@
+const path = require('path');
 const jwt = require('jsonwebtoken');
 
-const secret = 'secret_key';
+const readPath = path.resolve('jwt.evaluation.key');
+
+// const secret = 'secret_key';
+const jwtKey = require('fs')
+.readFileSync(readPath, { encoding: 'utf-8' });
 
 const JWT_CONFIG = {
   algorithm: 'HS256',
   expiresIn: '10d',
 };
 
-const createToken = (data) => jwt.sign({ data }, secret, JWT_CONFIG);
+const createToken = (data) => jwt.sign({ data }, jwtKey, JWT_CONFIG);
 
-const verifyToken = (token) => jwt.verify(token, secret);
+const verifyToken = (token) => jwt.verify(token, jwtKey);
 
 // console.log(!!verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjozLCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJwYXNzd29yZCI6IjFjMzc0NjZjMTU5NzU1Y2UxZmExODFiZDI0N2NiOTI1Iiwicm9sZSI6ImN1c3RvbWVyIn0sImlhdCI6MTY4MzY1ODM4MSwiZXhwIjoxNjg0NTIyMzgxfQ.jVUCugXoBJrrx15HIIaFOXO6ucS_GbUW36Uaf_GG98o', 'secret_key'));
 
