95afb153978a801f6e224ad07480c1a527acd468
josiel <josiel.jcc@hotmail.com>
2023-05-09 18:24:25 -0300

    feat: add cart to localstorage"

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,14 +1,44 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import products from '../tests/mocks/products.mocks';
+// import products from '../tests/mocks/products.mocks';
+import { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
+import ProductCard from '../components/ProductCard';
 
 function CustomerProducts() {
-  const [quantities, setQuantities] = useState(0);
-  const [valorTotal,
-    // setValorTotal
-  ] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
   const history = useHistory();
+
+  useEffect(() => {
+    const response = async () => {
+      const result = await fetch('http://localhost:3001/product');
+      const data = await result.json();
+      setProducts(data);
+    };
+    response();
+  }, []);
+
+  const addCart = async (id, quantity) => {
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || products;
+    const updatedItems = cartItems.map((product) => {
+      if (product.id === id) {
+        return { ...product, quantity };
+      }
+      return product;
+    });
+    const totalPrice = updatedItems.reduce((acc, curr) => {
+      if (curr.quantity) {
+        return acc + curr.quantity * Number(curr.price);
+      }
+      return acc;
+    }, 0);
+    setTotal(totalPrice);
+    setCart(updatedItems);
+    localStorage.setItem('cart', JSON.stringify(updatedItems));
+  };
 
   const carrinho = () => {
     history.push('/customer/checkout');
@@ -19,38 +49,8 @@ function CustomerProducts() {
       <NavBar />
       <div>
         {
-          products.map(({ id, name, price, urlImage }) => (
-            <div key={ id }>
-              <img
-                src={ urlImage }
-                alt={ name }
-                data-testid={ `customer_products__img-card-bg-image-${id}` }
-                width="50"
-              />
-              <p data-testid={ `customer_products__element-card-title-${id}` }>{name}</p>
-              <p data-testid={ `customer_products__element-card-price-${id}` }>{price}</p>
-              <button
-                type="button"
-                data-testid={ `customer_products__button-card-rm-item-${id}` }
-                name={ name }
-                onClick={ () => setQuantities(quantities < 1 ? 0 : quantities - 1) }
-              >
-                -
-              </button>
-              <input
-                type="text"
-                data-testid={ `customer_products__input-card-quantity-${id}` }
-                value={ quantities }
-              />
-              <button
-                type="button"
-                data-testid={ `customer_products__button-card-add-item-${id}` }
-                name={ name }
-                onClick={ () => setQuantities(quantities + 1) }
-              >
-                +
-              </button>
-            </div>
+          products.map((product) => (
+            <ProductCard key={ product.id } addCart={ addCart } product={ product } />
           ))
         }
       </div>
@@ -59,11 +59,13 @@ function CustomerProducts() {
         data-testid="customer_products__button-cart"
         onClick={ carrinho }
       >
-        <div
+        Ver Carrinho: R$
+        {' '}
+        <span
           data-testid="customer_products__checkout-bottom-value"
         >
-          {`Ver Carrinho: R$ ${valorTotal}`}
-        </div>
+          {JSON.stringify(total).replace('.', ',')}
+        </span>
       </button>
     </div>
   );
