edb36fba3fbf7c05230a3dc1ffce97c6ceb4ccb9
josiel <josiel.jcc@hotmail.com>
2023-05-09 15:59:23 -0300

    debug: testing req 13

--- back-end/src/api/auth/authFunctions.js
+++ back-end/src/api/auth/authFunctions.js
@@ -11,4 +11,6 @@ const createToken = (data) => jwt.sign({ data }, secret, JWT_CONFIG);
 
 const verifyToken = (token) => jwt.verify(token, secret);
 
+// console.log(!!verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjozLCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJwYXNzd29yZCI6IjFjMzc0NjZjMTU5NzU1Y2UxZmExODFiZDI0N2NiOTI1Iiwicm9sZSI6ImN1c3RvbWVyIn0sImlhdCI6MTY4MzY1ODM4MSwiZXhwIjoxNjg0NTIyMzgxfQ.jVUCugXoBJrrx15HIIaFOXO6ucS_GbUW36Uaf_GG98o', 'secret_key'));
+
 module.exports = { createToken, verifyToken };
