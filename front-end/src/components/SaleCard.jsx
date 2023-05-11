51cdc6baff7ab63f3469682dc0360a1521bf85a1
alexandrebelem21 <alexandrebelem21@gmail.com>
2023-05-11 16:05:53 -0300

    Fix: SaleCard dataTestId

--- front-end/src/components/SaleCard.jsx
+++ front-end/src/components/SaleCard.jsx
@@ -4,18 +4,19 @@ import formatPrice from '../utils/formatPrice';
 
 export default function SaleCard({ id, status, saleDate, totalPrice }) {
   return (
+
     <div className="sale-card">
-      <p data-testid={ `customer_orders__element-order-id${id}>` }>
+      <p data-testid={ `customer_orders__element-order-id-${id}` }>
         { id }
       </p>
-      <p data-testid={ `customer_orders__element-delivery-status-${id}>` }>
+      <p data-testid={ `customer_orders__element-delivery-status-${id}` }>
         { status }
       </p>
-      <p data-testid={ `customer_orders__element-order-date-${id}>` }>
+      <p data-testid={ `customer_orders__element-order-date-${id}` }>
         { new Date(Date.parse(saleDate))
           .toLocaleDateString('pt-BR', { timezone: 'UTC' }) }
       </p>
-      <p data-testid={ `customer_orders__element-card-price-${id}>` }>
+      <p data-testid={ `customer_orders__element-card-price-${id}` }>
         { formatPrice(totalPrice) }
       </p>
     </div>
--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -14,6 +14,7 @@ function CustomerOrders() {
     }
     getSales(endpoint);
   }, []);
+
   return (
     <div>
       <NavBar />
