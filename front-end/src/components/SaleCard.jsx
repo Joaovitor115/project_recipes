311944e19f05b885cfb33fa95d3c5cf51e173992
josiel <josiel.jcc@hotmail.com>
2023-05-11 17:21:56 -0300

    feat: create Link to /customer/order/id in the sale card

--- front-end/src/components/SaleCard.jsx
+++ front-end/src/components/SaleCard.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import formatPrice from '../utils/formatPrice';
 
 export default function SaleCard({ id, status, saleDate, totalPrice }) {
   return (
-
-    <div className="sale-card">
-      <p data-testid={ `customer_orders__element-order-id-${id}` }>
-        { id }
-      </p>
-      <p data-testid={ `customer_orders__element-delivery-status-${id}` }>
-        { status }
-      </p>
-      <p data-testid={ `customer_orders__element-order-date-${id}` }>
-        { new Date(Date.parse(saleDate))
-          .toLocaleDateString('pt-BR', { timezone: 'UTC' }) }
-      </p>
-      <p data-testid={ `customer_orders__element-card-price-${id}` }>
-        { formatPrice(totalPrice) }
-      </p>
-    </div>
+    <Link to={ `/customer/orders/${id}` }>
+      <div className="sale-card">
+        <p data-testid={ `customer_orders__element-order-id-${id}` }>
+          { id }
+        </p>
+        <p data-testid={ `customer_orders__element-delivery-status-${id}` }>
+          { status }
+        </p>
+        <p data-testid={ `customer_orders__element-order-date-${id}` }>
+          { new Date(Date.parse(saleDate))
+            .toLocaleDateString('pt-BR', { timezone: 'UTC' }) }
+        </p>
+        <p data-testid={ `customer_orders__element-card-price-${id}` }>
+          { formatPrice(totalPrice) }
+        </p>
+      </div>
+    </Link>
 
   );
 }
