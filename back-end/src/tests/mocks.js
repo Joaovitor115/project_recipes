05ff13279f311b7097575d6389efa6c4fe498d1c
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-05-12 19:04:07 -0300

    testes back end

---
+++ back-end/src/tests/mocks.js
@@ -0,0 +1,6 @@
+export const returnPostLogin = {
+  "name": "Cliente Zé Birita",
+  "email": "zebirita@email.com",
+  "role": "customer",
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjozLCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJwYXNzd29yZCI6IjFjMzc0NjZjMTU5NzU1Y2UxZmExODFiZDI0N2NiOTI1Iiwicm9sZSI6ImN1c3RvbWVyIn0sImlhdCI6MTY4MzkyODMwMSwiZXhwIjoxNjg0NzkyMzAxfQ.46tcbcvlL-O7DT1ot8oSEignOsVoiXxgJZG1wZwBuIU"
+}
