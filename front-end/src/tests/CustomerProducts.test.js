c35ef0e6b5ab62e4d8bc4153f8831f3a46185231
DanielRohe86 <psicologo.rohe@gmail.com>
2023-05-15 18:20:32 -0300

    req42 - testes de front com 40%+

--- front-end/src/tests/CustomerProducts.test.js
+++ front-end/src/tests/CustomerProducts.test.js
@@ -1,6 +1,6 @@
 // import React from 'react';
-import { screen, waitFor, userEvent } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 // import renderWithRouter from './helpers/renderWithRouter';
 // import App from '../App';
 import renderPath from './helpers/renderPath';
@@ -9,7 +9,7 @@ const userEmail = 'zebirita@email.com';
 const userName = 'Cliente Zé Birita';
 // const userPassword = '$#zebirita#$';
 
-const pathCustomerProducts = 'customer/products';
+const pathCustomerProducts = '/customer/products';
 
 const mockUserData = {
   name: userName,
@@ -23,56 +23,67 @@ const mockProductsData = [
     name: 'Skol Lata 250ml',
     price: 2.20,
     url_image: 'http://localhost:3001/images/skol_lata_350ml.jpg',
+    id: 1,
   },
   {
     name: 'Heineken 600ml',
     price: 7.50,
     url_image: 'http://localhost:3001/images/heineken_600ml.jpg',
+    id: 2,
   },
   {
     name: 'Antarctica Pilsen 300ml',
     price: 2.49,
     url_image: 'http://localhost:3001/images/antarctica_pilsen_300ml.jpg',
+    id: 3,
   },
   {
     name: 'Brahma 600ml',
     price: 7.50,
     url_image: 'http://localhost:3001/images/brahma_600ml.jpg',
+    id: 4,
   },
   {
     name: 'Skol 269ml',
     price: 2.19,
     url_image: 'http://localhost:3001/images/skol_269ml.jpg',
+    id: 5,
   },
   {
     name: 'Skol Beats Senses 313ml',
     price: 4.49,
     url_image: 'http://localhost:3001/images/skol_beats_senses_313ml.jpg',
+    id: 6,
   },
   {
     name: 'Becks 330ml',
     price: 4.99,
     url_image: 'http://localhost:3001/images/becks_330ml.jpg',
+    id: 7,
   },
   {
     name: 'Brahma Duplo Malte 350ml',
     price: 2.79,
     url_image: 'http://localhost:3001/images/brahma_duplo_malte_350ml.jpg',
+    id: 8,
   },
   {
     name: 'Becks 600ml',
     price: 8.89,
     url_image: 'http://localhost:3001/images/becks_600ml.jpg',
+    id: 9,
   },
   {
     name: 'Skol Beats Senses 269ml',
     price: 3.57,
     url_image: 'http://localhost:3001/images/skol_beats_senses_269ml.jpg',
+    id: 10,
   },
   {
     name: 'Stella Artois 275ml',
     price: 3.49,
     url_image: 'http://localhost:3001/images/stella_artois_275ml.jpg',
+    id: 11,
   },
 ];
 
@@ -84,6 +95,10 @@ const mockProductsData = [
 //   const requestJson = await request.json();
 //   return requestJson.results;
 // };
+// const tenThousand = 10000;
+// const fiveThousand = 5000;
+
+// jest.setTimeout(tenThousand);
 
 describe('Testa a página customer products', () => {
   beforeEach(() => {
@@ -92,35 +107,32 @@ describe('Testa a página customer products', () => {
     // localStorage.setItem('pedidos', JSON.stringify([]));
   });
   afterEach(() => localStorage.clear());
+  const mockFetch = () => Promise.resolve({
+    json: () => Promise.resolve(mockProductsData),
+  });
   it('testa a renderização da rota', async () => {
+    // jest.setTimeout(fiveThousand);
     const spy = jest.spyOn(window, 'fetch').mockImplementation(() => Promise.resolve({
       json: () => Promise.resolve({ results: mockProductsData }),
     }));
     renderPath(pathCustomerProducts);
     expect(spy).toHaveBeenCalled();
-
     const cusProdNavBar = await screen.findByTestId(/customer_products__element-navbar-link-products/i);
     expect(cusProdNavBar).toBeInTheDocument();
+    window.fetch.mockRestore();
   });
-
   it('testa a renderização dos produtos', async () => {
-    const mockFetch = () => Promise.resolve({
-      json: () => Promise.resolve(mockProductsData),
-    });
     jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
     renderPath(pathCustomerProducts);
     await waitFor(async () => {
       const cardSkolLata = await screen.findByText(/Skol Lata 250ml/i);
-      expect(cardSkolLata).toBeInTheDocument();
-    });
-    global.fetch.mockRestore();
+      return expect(cardSkolLata).toBeInTheDocument();
+    }, { timeout: 5000 });
   });
+
   it('testa o clique no botão', async () => {
-    const mockFetch = () => Promise.resolve({
-      json: () => Promise.resolve(mockProductsData),
-    });
     jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
-    renderPath('/customer/products');
+    renderPath(pathCustomerProducts);
     await waitFor(async () => {
       const addButtonPlus = screen.getByTestId(
         'customer_products__button-card-add-item-1',
